36496ca9155155cd22d9d2f260197aebdd20f5f3
jerry <becomegoodguy@gmail.com>
2024-05-31 01:06:23 +0900

    fix: 테스트 수정

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -27,8 +27,8 @@ export class AppController {
   @Post('/question')
   async getQuestion(@Body() body: { text: string }) {
     console.log('question: ', body.text);
-    const responseMessage = `테스트 완료 보낸 질문은 ${body.text}`;
-    return { message: responseMessage };
+
+    return '테스트 완료!!!!';
   }
 
   // 사진 촬영 및 캡셔닝
