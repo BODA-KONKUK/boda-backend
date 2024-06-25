d8a2601ef4c99ce493c9e02ebbe3542a07cc6ab9
jerry <becomegoodguy@gmail.com>
2024-06-25 11:25:43 +0900

    feat: 모델 변경

--- src/app.service.ts
+++ src/app.service.ts
@@ -30,7 +30,7 @@ export class AppService {
   async visualQuestionAnswering(imageUrl, question): Promise<any> {
     const inference = new HfInference('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');
     const vqa = inference.endpoint(
-      'https://b3g5s4adsa4ng48i.us-east-1.aws.endpoints.huggingface.cloud',
+      'https://pmv2sq2r47wtie5t.us-east-1.aws.endpoints.huggingface.cloud',
     );
 
     const imageres = await fetch(imageUrl);
