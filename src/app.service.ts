1dde07a65f56b8bd561737b1cbf6dfea4bccd5c9
jerry <becomegoodguy@gmail.com>
2024-06-25 12:20:40 +0900

    feat: captioning 모델 변경

--- src/app.service.ts
+++ src/app.service.ts
@@ -48,34 +48,55 @@ export class AppService {
   }
 
   async captioning(imageUrl: string) {
-    const API_TOKEN = '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';
+    // const API_TOKEN = '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';
+
+    // const imageres = await fetch(imageUrl);
+    // const imageBlob = await imageres.blob();
+
+    // async function query(imageBlob) {
+    //   const response = await fetch(
+    //     'https://api-inference.huggingface.co/models/nlpconnect/vit-gpt2-image-captioning',
+    //     {
+    //       headers: {
+    //         Authorization: `Bearer ${API_TOKEN}`,
+    //         'Content-Type': 'application/json',
+    //       },
+    //       method: 'POST',
+    //       body: imageBlob,
+    //     },
+    //   );
+
+    //   const result = await response.json();
+    //   return result;
+    // }
+
+    // return query(imageBlob).then((response) => {
+    //   const result = response[0];
+    //   console.log('responseresult', response[0]);
+
+    //   return result.generated_text;
+    // });
+
+    const inference = new HfInference('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');
+    const capModel = inference.endpoint(
+      'https://ygy9i8jr7xhty9ws.us-east-1.aws.endpoints.huggingface.cloud',
+    );
 
     const imageres = await fetch(imageUrl);
     const imageBlob = await imageres.blob();
 
-    async function query(imageBlob) {
-      const response = await fetch(
-        'https://api-inference.huggingface.co/models/nlpconnect/vit-gpt2-image-captioning',
-        {
-          headers: {
-            Authorization: `Bearer ${API_TOKEN}`,
-            'Content-Type': 'application/json',
-          },
-          method: 'POST',
-          body: imageBlob,
-        },
-      );
+    const response = await capModel.request(
+      {
+        data: imageBlob,
+      },
+      {
+        taskHint: 'image-to-text',
+      },
+    );
 
-      const result = await response.json();
-      return result;
-    }
+    console.log('response', response);
 
-    return query(imageBlob).then((response) => {
-      const result = response[0];
-      console.log('responseresult', response[0]);
-
-      return result.generated_text;
-    });
+    return response;
   }
 
   extractTranslatedText(response) {
@@ -84,94 +105,6 @@ export class AppService {
     const startIndex = response.indexOf(startMarker) + startMarker.length;
     const endIndex = response.indexOf(endMarker, startIndex);
     return response.substring(startIndex, endIndex);
-  }
-
-  async korToEng(questionInKor: string) {
-    async function query(questionInKor) {
-      try {
-        const response = await fetch(
-          'https://api-inference.huggingface.co/models/mistralai/Mistral-7B-Instruct-v0.3',
-          {
-            headers: {
-              Authorization: 'Bearer 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',
-              'Content-Type': 'application/json',
-            },
-            method: 'POST',
-            body: JSON.stringify(questionInKor),
-          },
-        );
-
-        // Check if the response is ok (status in the range 200-299)
-        if (!response.ok) {
-          const errorText = await response.text();
-          throw new Error(
-            `HTTP error! status: ${response.status}, body: ${errorText}`,
-          );
-        }
-
-        const result = await response.json();
-        return result;
-      } catch (error) {
-        console.error('Error during fetching or parsing:', error);
-        throw error;
-      }
-    }
-
-    return await query({
-      inputs: `Translate into English in a sentence: '${questionInKor}'
-      `,
-    })
-      .then((response) => {
-        console.log(response);
-        // console.log(this.extractTranslatedText(response));
-        return response;
-      })
-      .catch((error) => {
-        console.error('Query failed:', error);
-      });
-  }
-
-  async engToKor(questionInEng: string) {
-    async function query(questionInEng) {
-      try {
-        const response = await fetch(
-          'https://api-inference.huggingface.co/models/mistralai/Mistral-7B-Instruct-v0.3',
-          {
-            headers: {
-              Authorization: 'Bearer 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',
-              'Content-Type': 'application/json',
-            },
-            method: 'POST',
-            body: JSON.stringify(questionInEng),
-          },
-        );
-
-        // Check if the response is ok (status in the range 200-299)
-        if (!response.ok) {
-          const errorText = await response.text();
-          throw new Error(
-            `HTTP error! status: ${response.status}, body: ${errorText}`,
-          );
-        }
-
-        const result = await response.json();
-        return result;
-      } catch (error) {
-        console.error('Error during fetching or parsing:', error);
-        throw error;
-      }
-    }
-
-    return await query({
-      inputs: `Translate into Korean: "${questionInEng}"`,
-    })
-      .then((response) => {
-        // console.log(JSON.stringify(response));
-        return response;
-      })
-      .catch((error) => {
-        console.error('Query failed:', error);
-      });
   }
 
   async vqa(imgUrl: string, question: string): Promise<string> {
